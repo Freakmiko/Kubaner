88023100c7bbe5e32d052095054cba27bfe84291
Freakmiko <freakmiki@gmail.com>
2015-06-12 12:54:23 +0200

    Subjects are now stored in lexicographical order

--- src/Kubaner/Logic/SubjectList.java
+++ src/Kubaner/Logic/SubjectList.java
@@ -12,7 +12,12 @@ public class SubjectList {
     }
 
     void add(Subject subject) {
-        subjects.add(subject);
+        for(int i = 0; i < size(); i++) {
+            if(subjects.get(i).getName().compareTo(subject.getName()) >= 1) {
+                subjects.add(subject);
+                break;
+            }
+        }
     }
 
     public Subject create(String name) {
