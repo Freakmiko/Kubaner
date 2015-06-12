8d0e4d65a6d48738584b504d30c7203d1d003dd5
Freakmiko <freakmiki@gmail.com>
2015-06-12 12:52:07 +0200

    Created SubjectList

--- src/Kubaner/Logic/SubjectList.java
+++ src/Kubaner/Logic/SubjectList.java
@@ -1,5 +1,46 @@
 package Kubaner.Logic;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class SubjectList {
 
+    List<Subject> subjects;
+
+    public SubjectList() {
+        subjects = new LinkedList<>();
+    }
+
+    void add(Subject subject) {
+        subjects.add(subject);
+    }
+
+    public Subject create(String name) {
+        Subject newSubject = new Subject(name);
+        subjects.add(newSubject);
+        return newSubject;
+    }
+
+    public Subject get(int index) {
+        return subjects.get(index);
+    }
+
+    public Subject[] toArray() {
+        return (Subject[])subjects.toArray();
+    }
+
+    public boolean exist(Subject subject) {
+        return subjects.contains(subject);
+    }
+
+    public boolean delete(int index) {
+        if(index < 0 || index >= size())
+            return false;
+        subjects.remove(index);
+        return true;
+    }
+
+    public int size() {
+        return subjects.size();
+    }
 }
