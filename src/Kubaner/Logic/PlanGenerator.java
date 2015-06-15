b746e4273a8c80a681cbaa04ee3c5b019ae7c0bf
Pentomatics <s.brehm1@gmx.de>
2015-06-15 17:55:58 +0200

    Added PlanGenerator methods

--- src/Kubaner/Logic/PlanGenerator.java
+++ src/Kubaner/Logic/PlanGenerator.java
@@ -1,5 +1,11 @@
 package Kubaner.Logic;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
@@ -10,6 +16,124 @@ public class PlanGenerator {
 	private StudentList studentList;
 	private SubjectList subjectList;
 	private ProfList profList;
+	
+	
+	PlanGenerator() {
+		studentList = new StudentList();
+		subjectList = new SubjectList();
+		profList = new ProfList();
+	}
+	
+	public StudentList getStudentList() {
+		return studentList;
+	}
+	
+	
+	public SubjectList getSubjectList() {
+		return subjectList;
+	}
+	
+	
+	public ProfList getProfList() {
+		return profList;
+	}
+	
+	
+	/**
+	 * The argument plan will be stored in a file described by the argument url.
+	 * But if plan is null, the current lists of this class will be stored.
+	 * This concerns the following lists: studentList, subjectList, profList.
+	 * @param plan - the plan to store.
+	 * @param url - the url of the file, where the data should be stored.
+	 * @throws IOException - if there are problems with the url.
+	 */
+	public void savePlan(Plan plan, String url) throws IOException {
+		ObjectOutputStream oos = new ObjectOutputStream (new FileOutputStream(url));
+		
+		if(plan != null) {
+			oos.writeObject(plan);
+			oos.close();
+		
+		//save studentList, subjectList, profList
+		} else {
+			oos.writeObject(studentList);
+			oos.writeObject(subjectList);
+			oos.writeObject(profList);
+			
+			oos.close();
+		}
+	}
+	
+	
+	/**
+	 * The data stored in a file described by the argument url will be restored.
+	 * If an instance of the class plan is stored in the file, the plan will be restored
+	 * and the lists for this generator will be restored from the information stored in the plan.
+	 * But maybe there is no plan in the file. In this case the program will try to restore
+	 * the 3 lists directly from the file.
+	 * @param url - the url of the file, where the data should be stored.
+	 * @return Returns the plan restored from the file or null if there was no plan stored in the file.
+	 * @throws IOException - if there are problems with the url.
+	 */
+	public Plan loadPlan(String url) throws IOException, ClassNotFoundException {
+		Plan plan = null;
+		Object obj;
+		ObjectInputStream ois = new ObjectInputStream (new FileInputStream(url));
+		
+		
+		obj = ois.readObject();
+		
+		if(obj != null)
+			if(obj.getClass() == Plan.class) {
+				plan = (Plan)obj;
+				restoreLists(plan);
+			} else {
+				studentList = (StudentList)obj;
+				subjectList = (SubjectList)ois.readObject();
+				profList = (ProfList)ois.readObject();
+			}
+		
+		ois.close();
+		return plan;
+	}
+	
+	
+	private void restoreLists(Plan plan) {
+		TimeLine timeline;
+		TimeLineMember member;
+		Exam exam;
+		
+		Student student;
+		Subject subjects[];
+		Professor prof[];
+		
+		
+		for(int i = 0; i < plan.getTimeLineNumber(); i++) {
+			timeline = plan.getTimeLine(i);
+			
+			for(int i2 = 0; i2 < timeline.getTimeLineMemberNumber(); i2++) {
+				member = timeline.getTimeLineMember(i2);
+				
+				if(member.getClass() == new Exam().getClass()) {
+					exam = (Exam)member;
+					subjects = exam.getSubjectArray();
+					
+					for(int i3 = 0; i3 < subjects.length; i3++) {
+						this.subjectList.create(subjects[i3].getName());
+					}
+					
+					student = exam.getStudent();
+					this.studentList.create(student.getName(), student.getSubjectArray(), student.getTimePeriodArray());
+					
+					prof = exam.getProfessorArray();
+					
+					for(int i3 = 0; i3 < prof.length; i3++) {
+						this.profList.create(prof[i3].getName(), prof[i3].getSubjectArray(), prof[i3].getTimePeriodArray());
+					}
+				}
+			}
+		}
+	}
 	
 	
 	public Plan generatePlan(Date startTime)
