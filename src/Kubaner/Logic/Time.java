012873136c5ffce27f0065cbc5911f1f085d53b0
Soesti <maxsoest@gmail.com>
2015-06-17 12:24:37 +0200

    englischer Text, Methoden 	isEarlierOrEqual 	isLaterOrEqual 	equals 	addMinutes
    hinzugefügt

--- src/Kubaner/Logic/Time.java
+++ src/Kubaner/Logic/Time.java
@@ -2,28 +2,55 @@ package Kubaner.Logic;
 
 public class Time {
 	
-	private int Stunde;
-	private int Minute;
+	private int hour;
+	private int minute;
 	
-	public Time(int stunde, int minute){
-		this.Stunde=stunde;
-		this.Minute=minute;
+	public Time(int hour, int minute){
+		this.hour=hour;
+		this.minute=minute;
 	}
 	
-	public int getStunde() {
-		return Stunde;
+	public int getHour() {
+		return hour;
 	}
 
-	public void setStunde(int stunde) {
-		Stunde = stunde;
+	public void setHour(int stunde) {
+		hour = stunde;
 	}
 	
 	public int getMinute() {
-		return Minute;
+		return minute;
 	}
 
 	public void setMinute(int minute) {
-		Minute = minute;
+		this.minute = minute;
+	}
+	
+	
+	/**
+	 * Adds an positive amount of minutes to current time of the current {@link Time} object
+	 * and returns a new Time object which represents the new time.
+	 * 
+	 * @param additionalMinutes
+	 * 		The amount of minutes to be added.
+	 * @return
+	 * 		A new Time object which contains the new time value.
+	 * 
+	 * @throws IllegalArgumentException
+	 * 		if the additionalTime parameter is smaller than 0.
+	 */
+	public Time addMinutes(int additionalMinutes)
+	{
+		if(additionalMinutes < 0)
+			throw new IllegalArgumentException("Cannot add a negative amount of minutes.");
+		
+		int newMinutes = (this.minute + additionalMinutes) % 60;
+		int additionalHours = (this.minute + additionalMinutes) / 60;
+		int newHour = (this.hour + additionalHours) % 24;
+		
+		Time newTime = new Time(newHour, newMinutes);
+		
+		return newTime;
 	}
 	
 	/**
@@ -32,10 +59,10 @@ public class Time {
 	 * @param t Time
 	 */
 	public boolean isEarlier(Time t){
-		if(Stunde<t.Stunde)
+		if(hour<t.hour)
 			return true;
-		else if(t.Stunde==this.Stunde)
-			if(Minute<t.Minute)
+		else if(t.hour==this.hour)
+			if(minute<t.minute)
 				return true;			
 		
 		return false;
@@ -47,13 +74,61 @@ public class Time {
 	 * @param t Time
 	 */
 	public boolean isLater(Time t){
-		if(Stunde>t.Stunde)
+		if(hour>t.hour)
 			return true;
-		else if(Stunde>t.Stunde)
-			if(Minute>t.Minute)
+		else if(hour>t.hour)
+			if(minute>t.minute)
 				return true;
 			
 		return false;
 	}
+	
+	/**
+	 * Checks if another time object is later or equal to another Time object.
+	 * 
+	 * @param another
+	 * 		Another Time object.
+	 * @return
+	 * 		true, if this Time is equal or later than the parameter Time, otherwise false.
+	 */
+	public boolean isLaterOrEqual(Time another)
+	{
+		return equals(another) || isLater(another);
+	}
+	
+	/**
+	 * Checks if another time object is earlier or equal to another Time object.
+	 * 
+	 * @param another
+	 * 		Another Time object.
+	 * @return
+	 * 		true, if this Time is equal or earlier than the parameter Time, otherwise false.
+	 */
+	public boolean isEarlierOrEqual(Time another)
+	{
+		return equals(another) || isEarlier(another);
+	}
 
+	/**
+	 * Checks if another time object is equal to the current Time object.
+	 * 
+	 * @param anotherTime
+	 * 		A time object.
+	 * @return
+	 * 		True, if the hour and the minutes of both Time objects are equal, otherwise false.
+	 */
+	public boolean equals(Time anotherTime)
+	{
+		if(this == anotherTime)
+			return true;
+		else if(anotherTime == null)
+			return false;
+		else if(this.minute == anotherTime.minute
+				&& this.hour == anotherTime.hour)
+		{
+			return true;
+		}
+		
+		return false;
+	}
 }
