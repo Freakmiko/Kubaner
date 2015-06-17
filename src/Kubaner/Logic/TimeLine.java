3e46e2a80b5f06a3b497d87005425d1dfe62c1c7
Soesti <maxsoest@gmail.com>
2015-06-17 12:54:45 +0200

    Implementierung geändert von Array zu Liste

--- src/Kubaner/Logic/TimeLine.java
+++ src/Kubaner/Logic/TimeLine.java
@@ -1,61 +1,36 @@
 package Kubaner.Logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimeLine {
 	
 	public String room;
-	private TimeLineMember[] timeLine;
+	private List<TimeLineMember> memberList;
 	
-	
-	public TimeLine(int timeLineMemberCount, String room) {
-		this.timeLine = new TimeLineMember[timeLineMemberCount];
-		this.room = room;
+	public TimeLine(){
+		memberList = new ArrayList<TimeLineMember>();
 	}
 	
-	public TimeLine(String room) {
-		this.timeLine = new TimeLineMember[96]; // 24 Stunden, je 15 Minuten
-		this.room = room;
+	public void add(TimeLineMember member){
+		memberList.add(member);
 	}
 	
-	public TimeLine(int timeLineMemberCount) {
-		this.timeLine = new TimeLineMember[timeLineMemberCount];
-		this.room = "Testraum";
+	public void insert(TimeLineMember member, int index){
+		memberList.add(index, member);
 	}
 	
-	public TimeLine() {
-		this.timeLine = new TimeLineMember[96]; // 24 Stunden, je 15 Minuten
-		this.room = "Testraum";
+	public void delete(int index){
+		memberList.remove(index);
 	}
 	
-	
-	public void insertAt(int index, TimeLineMember time){
-		this.timeLine[index] = time;		
-	}
-	
-	public void deleteAt(int index){
-		this.timeLine[index] = null;
+	public int size(){
+		return memberList.size();
 	}
 	
 	public void moveMember(int oldIndex, int newIndex){
-		
-		//es fehlen hier noch Exceptions und andere ausnahmen
-		
-		TimeLineMember tmpOld = this.timeLine[oldIndex];
-		if(newIndex > oldIndex){
-			for(int i = oldIndex; i == newIndex; i++){
-				this.timeLine[i] = this.timeLine[i+1];
-			}
-			this.timeLine[newIndex] = tmpOld;
-		}
-		if(newIndex < oldIndex){
-			for(int i = oldIndex; i == newIndex; i--){
-				this.timeLine[i] = this.timeLine[i-1];
-			}
-			this.timeLine[newIndex] = tmpOld;	
-		}
-		
-		if(newIndex == oldIndex){
-		}
-		this.timeLine[newIndex] = this.timeLine[oldIndex];
+		TimeLineMember help = memberList.remove(oldIndex);
+		memberList.add(newIndex, help);
 	}
 
 	public String getRoom(){
@@ -66,18 +41,8 @@ public class TimeLine {
 		this.room = room;
 	}
 	
-	public int elementCount(){
-		int count = 0;
-		for(int i = 0; i >= timeLine.length; i++){
-			if(timeLine[i]!=null){
-				count++;
-			}
-		}
-		return count;
-	}
-	
 	public TimeLineMember getTimeLineMember(int index){
-		return this.timeLine[index];
+		return memberList.get(index);
 	}
 	
 
