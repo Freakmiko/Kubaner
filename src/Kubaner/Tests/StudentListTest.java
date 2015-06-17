1965a8e3262f851fcbb4f64212a585edc3da1420
Freakmiko <freakmiki@gmail.com>
2015-06-17 23:58:02 +0200

    StudentList tests added.

---
+++ src/Kubaner/Tests/StudentListTest.java
@@ -0,0 +1,98 @@
+package Kubaner.Tests;
+
+import Kubaner.Logic.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentListTest {
+    StudentList studentList;
+    SubjectList subjectList;
+
+    @Before
+    public void setup() {
+        subjectList = new SubjectList();
+        subjectList.create("ANA");
+        subjectList.create("GTI");
+        studentList = new StudentList();
+    }
+
+    @Test
+    public void testCreateProfessor() throws Exception {
+        Student student = studentList.create("Kubaner", subjectList.toArray(),new TimePeriod[] {new TimePeriod(new Time(0,0), new Time(12,0))});
+        assertEquals("Kubaner", student.getName());
+        // TODO: Sollen hier noch die Faecher und die Zeiten ueberprueft werden?
+    }
+
+    @Test
+    public void testCreateWithTwoProfessorsCreateOneInbetween() throws Exception {
+        // TODO: Das ist sollte nicht so getestet werden! (Momentan kein anderer Weg)
+        studentList.create("Kubaner", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        studentList.create("Todorov", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        studentList.create("Schramm", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        assertEquals("Schramm", studentList.get(1).getName());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetFromEmptyList() throws Exception {
+        studentList.get(1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetOutOfBounds() throws Exception {
+        studentList.create("Kubaner", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        studentList.get(1);
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        studentList.create("Kubaner", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        assertEquals("Kubaner", studentList.get(0).getName());
+    }
+
+    @Test
+    public void testDeleteFromEmptyList() throws Exception {
+        assertFalse(studentList.delete(0));
+    }
+
+    @Test
+    public void testDeleteWithOneElement() throws Exception {
+        studentList.create("Kubaner", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        assertTrue(studentList.delete(0));
+    }
+
+    @Test
+    public void testDeleteBetweenElements() throws Exception {
+        studentList.create("Kubaner", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        studentList.create("Todorov", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        studentList.create("Schramm", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        assertTrue(studentList.delete(1));
+        assertEquals("Todorov", studentList.get(1).getName());
+    }
+
+    @Test
+    public void testExists() throws Exception {
+        Student student = studentList.create("Kubaner", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        assertTrue(studentList.exists(student));
+    }
+
+    @Test
+    public void testSize() throws Exception {
+        studentList.create("Kubaner", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        studentList.create("Todorov", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        studentList.create("Schramm", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        assertEquals(3, studentList.size());
+    }
+
+    @Test
+    public void testToArray() throws Exception {
+        studentList.create("Kubaner", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        studentList.create("Todorov", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        studentList.create("Schramm", subjectList.toArray(), new TimePeriod[]{new TimePeriod(new Time(0, 0), new Time(12, 0))});
+        Student[] arr = studentList.toArray();
+        assertEquals("Kubaner", arr[0].getName());
+        assertEquals("Schramm", arr[1].getName());
+        assertEquals("Todorov", arr[2].getName());
+    }
+}
