2ce0036806a7f431d6f231eeb785851721c891b4
Thorsten <Thorsten@Thorsten-Tablet>
2015-06-18 14:11:27 +0200

    Bearbeitugsfenster erstellt

---
+++ src/Kubaner/GUI/ChangeMaskProfessor.java
@@ -0,0 +1,23 @@
+package Kubaner.GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import Kubaner.Logic.Plan;
+import Kubaner.Logic.PlanGenerator;
+
+public class ChangeMaskProfessor extends JFrame implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	ChangeMaskProfessor(Plan plan, PlanGenerator planGenerator){
+		
+	}
+
+}
---
+++ src/Kubaner/GUI/ChangeMaskSubject.java
@@ -0,0 +1,22 @@
+package Kubaner.GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import Kubaner.Logic.Plan;
+import Kubaner.Logic.PlanGenerator;
+
+public class ChangeMaskSubject extends JFrame implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	ChangeMaskSubject(Plan plan, PlanGenerator planGenerator){
+		
+	}
+}
