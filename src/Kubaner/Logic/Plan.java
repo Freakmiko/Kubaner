19f6c3ed44dcbf2636289053cc2a369b4dc1bc35
Pentomatics <s.brehm1@gmx.de>
2015-06-18 20:01:17 +0200

    Added methods to plan. Now createAbstractTableModel() is up!

--- src/Kubaner/Logic/Plan.java
+++ src/Kubaner/Logic/Plan.java
@@ -1,5 +1,6 @@
 package Kubaner.Logic;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Plan {
@@ -44,5 +45,325 @@ public class Plan {
 	 */
 	public TimeLine getTimeLine(int index) {
 		return this.timeline[index];
+	}
+	
+	
+	/**
+	 * Returns the number of the timelines as room count.
+	 * @return The room count.
+	 */
+	//room count is here number of timelines
+	public int getRoomCount() {
+		return timeline.length;
+	}
+	
+	
+	/**
+	 * Creates a new TimeLine.
+	 * @param room - the room for the timeline
+	 * @return TimeLine the created TimeLine.
+	 */
+	public TimeLine createNewTimeLine(String room) {
+		TimeLine timeline = new TimeLine();
+		
+		timeline.setRoom(room);
+		return timeline;
+	}
+	
+	
+	/**
+	 * Moves a timeline member from one to another timeline. If the argument timelines are the same,
+	 * the timeline member will be moved from the old to the new index within the timeline.
+	 * @param oldLine - the timeline with the timeline member to be moved.
+	 * @param oldIndex - the index of the member to be moved.
+	 * @param newLine - the timeline where the timeline member gets inserted.
+	 * @param newIndex - the index, where the member gets inserted.
+	 * @throws IllegalArgumentException - if a TimeLine is null or the index's are not compatible with the timelines.
+	 */
+	public void moveBetweenTimelines(TimeLine oldLine, int oldIndex, TimeLine newLine, int newIndex) throws IllegalArgumentException {
+		int oldLineIndex = -1;
+		int newLineIndex = -1;
+		
+		TimeLineMember member;
+		
+		if(oldLine == null || newLine == null)
+			throw new IllegalArgumentException("Timeline argument was null.");
+		
+		for(int i = 0; i < timeline.length; i++) {
+			if(timeline[i] == oldLine) {
+				oldLineIndex = i;
+			}
+			
+			if(timeline[i] == newLine) {
+				newLineIndex = i;
+			}
+		}
+		
+		
+		//if one of the argument lines does not exist
+		if(oldLineIndex == -1 || newLineIndex == -1) {
+			throw new IllegalArgumentException("Incompatible index.");
+		}
+		
+		
+		//special case - movePeriod within timeline
+		if(oldLine == newLine && oldIndex != newIndex) {
+			//oldLineIndex == newLineIndex
+			timeline[oldLineIndex].moveMember(oldIndex, newIndex);
+		}
+		
+		
+		member = timeline[oldLineIndex].getTimeLineMember(oldIndex);
+		timeline[oldLineIndex].delete(oldIndex);
+		
+		timeline[newLineIndex].insert(newIndex, member);
+	}
+	
+	
+	/**
+	 * Creates a MasterModel representing the master plan, using the values which are actually stored in this class.
+	 * @return the created MasterModel.
+	 */
+	//using deprecated methods
+	public MasterModel createAbstractTableModel() {
+		int rows = 0;
+		int cols;
+		
+		StudentList studentList = new StudentList();
+		SubjectList subjectList = new SubjectList();
+		ProfList profList = new ProfList();
+		
+		ArrayList<Time[]> startTimeList = new ArrayList<Time[]>();
+		MasterModel master;
+		Object value;
+		Time actualTime;
+		
+		TimeLineMember member;
+		Exam exam;
+		
+		Student student;
+		Subject subjects[];
+		Professor prof[];
+		
+		
+		//get nearly all information stored in the plan to work with it
+		for(int i = 0; i < timeline.length; i++) {
+			for(int i2 = 0; i2 < timeline[i].size(); i2++) {
+				member = timeline[i].getTimeLineMember(i2);
+				
+				if(member instanceof Exam) {
+					exam = (Exam)member;
+					subjects = exam.getSubjectArray();
+					
+					for(int i3 = 0; i3 < subjects.length; i3++)
+						subjectList.create(subjects[i3].getName());
+					
+					student = exam.getStudent();
+					studentList.create(student.getName(), student.getSubjectArray(), student.getTimePeriodArray());
+					
+					prof = exam.getProfArray();
+					
+					for(int i3 = 0; i3 < prof.length; i3++)
+						profList.create(prof[i3].getName(), prof[i3].getSubjectArray(), prof[i3].getTimePeriodArray());
+				}
+			}
+		}
+		
+		//generate startTimeList
+		startTimeList = createTimeList();
+		
+		//calculate master model size
+		rows = calculateRows(startTimeList);
+		
+		//+1 because first column stores start times
+		cols = subjectList.size() + 1;
+		
+		
+		master = new MasterModel(rows, cols);
+		actualTime = new Time(startTime.getHours(), startTime.getMinutes());
+		
+		
+		for(int i = 0; i < cols; i++) {
+			for(int i2 = 0; i2 < rows; i2++) {
+				//first column: start times
+				if(i == 0) {
+					if(i2 > 0) {
+						if(i2 > 1)
+							actualTime = getNextStartTime(actualTime, startTimeList);
+						//we will need the times to calculate other values in table model
+						//so don't store it as String yet
+						value = actualTime;
+					} else
+						value = null;
+					
+				//column > 0
+				} else {
+					if(i2 == 0) {
+						value = subjectList.get(i - 1).getName();
+						
+						for(int i3 = 0; i3 < profList.size(); i3++) {
+							subjects = profList.get(i3).getSubjectArray();
+							
+							for(int i4 = 0; i4 < subjects.length; i4++) {
+								if(subjects[i].getName() == subjectList.get(i - 1).getName())
+									value += "\n" + profList.get(i3).getName();
+							}
+						}	
+					
+					//row > 0
+					} else {
+						//value at i2, i is still an instance of the class Time
+						exam = getExamAtTime((Time)master.getValueAt(i2, i), subjectList.get(i - 1));
+						
+						if(exam != null) {
+							value = "" + exam.getStudent().getName() + "   " + findExamRoom(exam);
+						} else {
+							value = null;
+						}
+					}
+						
+				}
+				
+				master.setValueAt(value, i, i2);
+			}
+				
+		}
+		
+		//convert times from Time to String
+		for(int i = 1; i < rows; i++) {
+			value = master.getValueAt(i, 0);
+			if(value != null) {
+				Time time = (Time)value;
+				value = "" + time.getHour() + "." + time.getMinute();
+				master.setValueAt(value, i, 0);
+			}
+		}
+		
+		return master;
+	}
+	
+	
+	/**
+	 * Creates and returns an ArrayList<Time[]> which stores array with startTimes of every TimeLine object stored in timeline[].
+	 * @return Returns an ArrayList<Time[]> which stores array with startTimes of every TimeLine object stored in timeline[].
+	 */
+	//using deprecated methods
+	private ArrayList<Time[]> createTimeList() {
+		ArrayList<Time[]> list = new ArrayList<Time[]>();
+		Time[] timeArray;
+		
+		int nextDuration = 0;
+		
+		Time actualTime;
+		
+		for(int i = 0; i < timeline.length; i++) {
+			actualTime = new Time(startTime.getHours(), startTime.getMinutes());
+			timeArray = new Time[timeline[i].size()];
+			
+			timeArray[0] = new Time(startTime.getHours(), startTime.getMinutes());
+			
+			for(int i2 = 0; i2 < timeline[i].size(); i2++) {
+				nextDuration = timeline[i].getTimeLineMember(i2).getLength();
+				actualTime = actualTime.addMinutes(nextDuration);
+				timeArray[i2] = new Time(actualTime.getHour(), actualTime.getMinute());
+			}
+			list.add(timeArray);
+		}
+		return list;
+	}
+	
+	
+	/**
+	 * Determines and returns the next starting time of the argument startTimeList.
+	 * @return Returns the next starting time of the argument startTimeList
+	 * 			or null if there is no start time later than priorTime.
+	 */
+	private Time getNextStartTime(Time priorTime, ArrayList<Time[]> startTimeList) {
+		Time nextTime = priorTime;
+		
+		Time actualTime;
+		
+		for(int i = 0; i < startTimeList.size(); i++) {
+			for(int i2 = 0; i2 < startTimeList.get(i).length; i2++) {
+				actualTime = startTimeList.get(i)[i2];
+				
+				if(actualTime.isLater(priorTime)) {
+					if(actualTime.isEarlier(nextTime))
+						nextTime = actualTime;
+					break;
+				}
+			}
+		}
+		
+		if(nextTime == priorTime)
+			return null;
+		
+		return nextTime;
+	}
+	
+	
+	/**
+	 * Calculates the number of rows the master model should have.
+	 * @return Returns the number of rows the master model should have.
+	 */
+	//using deprecated methods
+	private int calculateRows(ArrayList<Time[]> startTimeList) {
+		int rows = 1;
+		
+		Time time = new Time(startTime.getHours(), startTime.getMinutes());
+		
+		while(time != null) {
+			
+			time = getNextStartTime(time, startTimeList);
+			
+			if(time != null)
+					rows++;
+		}
+		return rows;
+	}
+	
+	
+	/**
+	 * Searches for the Exam, which is taking place at a certain time. The subject of this exam is needed
+	 * to find the right exam, because there could be more exams at the same time.
+	 * @return The found exam or null if no exam was found.
+	 */
+	private Exam getExamAtTime(Time time, Subject sub) {
+		TimeLineMember member;
+		Exam exam;
+		
+		Subject[] subjects;
+		
+		for(int i = 0; i < timeline.length; i++) {
+			for(int i2 = 0; i2 < timeline[i].size(); i2++) {
+				member = timeline[i].getTimeLineMember(i2);
+				if(member instanceof Exam) {
+					exam = (Exam)member;
+					subjects = exam.getSubjectArray();
+					
+					for(int i3 = 0; i3 < subjects.length; i3++)
+						if(subjects[i3].getName().equals(sub.getName())) {
+							return exam;
+						}
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	
+	/**
+	 * Searches for the room, in which the argument exam is taking place.
+	 * @return The room name, or "" if no room was found.
+	 */
+	private String findExamRoom(Exam exam) {
+		for(int i = 0; i < timeline.length; i++) {
+			for(int i2 = 0; i2 < timeline[i].size(); i2++) {
+				if(timeline[i].getTimeLineMember(i2) == exam)
+					return timeline[i].getRoom();
+			}
+		}
+		return "";
 	}
 }
