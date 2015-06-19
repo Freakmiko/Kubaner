50b9df5cec106e69a54f402b399785cc9be1aae0
SimonBrehm <Brehm.simon@Gmail.com>
2015-06-20 00:36:42 +0200

    Update overviewMaskProfessor.java
    Würde funktionieren, wenn die Funktionen getSubjectArray() und getTimePeriodArray() der Klasse Prof funtionieren würde

--- src/Kubaner/GUI/overviewMaskProfessor.java
+++ src/Kubaner/GUI/overviewMaskProfessor.java
@@ -9,18 +9,33 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.TableModel;
 
 public class overviewMaskProfessor extends JFrame implements ActionListener {
 	private JTable table;
 	private JButton exitButton;
 	private JPanel south;
-	
+	ProfList list;
 	public overviewMaskProfessor(PlanGenerator planGenerator) {
+		list = planGenerator.getProfList();
 		getContentPane().setLayout(new BorderLayout());
 		setTitle("Professoren Uebersicht");
-		setSize(200, 200);
+		setSize(400, 400);
 		setLocationRelativeTo(null);
+		
+		TableModel dataModel = new DataModel(list.size(),3);
+		table = new JTable(dataModel);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		add( new JScrollPane( table ), BorderLayout.CENTER );
+		
+		for ( int row = 0; row < list.size(); row++ )
+			for ( int col = 0; col < 1; col++ ) {
+				dataModel.setValueAt(list.get(row).getName(), row, col);
+				dataModel.setValueAt(subjectsString(list.get(row).getSubjectArray()), row, col+1);
+				dataModel.setValueAt(TimePeriodString(list.get(row).getTimePeriodArray()), row, col+2);
+			}
 		
 		//Knopf anlegen
 		south = new JPanel();
@@ -32,8 +47,36 @@ public class overviewMaskProfessor extends JFrame implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(e.getSource() == exitButton) {
+			setVisible(false);
+			dispose();
+		}
+	}
+	
+	/**
+	 * Function which creates a String with all subjects of a student.
+	 * @param subjects list of subjects.
+	 * @return A String with all subjects of a student.
+	 */
+	String subjectsString(Subject[] subjects) {
+		String returnString = "";
+		for(int i = 0; i < subjects.length; i++) {
+			returnString += subjects[i].getName() + ", ";
+		}
+		return returnString;
+	}
+	
+	/**
+	 * Function which creates a String with all timeperiods of a student.
+	 * @param period list of timeperiods.
+	 * @return A String with all timeperiods of a student.
+	 */
+	String TimePeriodString(TimePeriod[] period) {		
+		String returnString = "";
+		for(int i = 0; i < period.length; i++) {
+			returnString += "start: " + period[i].getStart().getHour() + ":" + period[i].getStart().getMinute() + " end: " + period[i].getEnd().getHour() + ":" + period[i].getEnd().getMinute() + ", ";
+		}
+		return returnString;
 	}
 
 }
