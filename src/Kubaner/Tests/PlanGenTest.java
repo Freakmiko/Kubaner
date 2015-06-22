9f2787af2c3bb92d5fb2665f53c618e2e984f760
justeverything61 <justeverything@web.de>
2015-06-22 23:32:02 +0200

    new test added, more javadoc and comments

--- src/Kubaner/Tests/PlanGenTest.java
+++ src/Kubaner/Tests/PlanGenTest.java
@@ -13,10 +13,16 @@ public class PlanGenTest {
 	ProfList profList;
 	SubjectList subList;
 	StudentList stuList;
-	Subject[] allSubjectsArray = new Subject[5]; // constructor for array containing every subject
-	Subject[] arraySubjectsKubaner = new Subject[2]; // kubaner subject array (1 subject)
-	Subject[] arraySubjectsTodorov = new Subject[1]; // todorov subject array (2 subjects)
-	Subject[] arraySubjectsSchramm = new Subject[2]; // schramm subject array (2 subjects)
+	
+	//constructor for array containing every subject
+	Subject[] allSubjectsArray = new Subject[5]; 
+	
+	//Kubaner has 2 subjects, Todorov has 1 subject, Schramm has 2 subjects
+	Subject[] arraySubjectsKubaner = new Subject[2]; 
+	Subject[] arraySubjectsTodorov = new Subject[1]; 
+	Subject[] arraySubjectsSchramm = new Subject[2]; 
+	
+	//subject arrays for the new students
 	Subject[] arraySubjectsStu1 = new Subject[3];
 	Subject[] arraySubjectsStu2 = new Subject[2];
 	Subject[] arraySubjectsStu3 = new Subject[4];
@@ -28,51 +34,66 @@ public class PlanGenTest {
 		profList = alpha.getProfList();
 		subList = alpha.getSubjectList();
 		stuList = alpha.getStudentList();
+							   
+		// the subjects        //position of subjects in allSubjectsArray, alphabetical order!
 		subList.create("ADS"); // 0
 		subList.create("ANA"); // 1
 		subList.create("GDI"); // 2
 		subList.create("OOT"); // 3
 		subList.create("TPE"); // 4
 		
-		//creating array containing all subjects
+		//writing all subjects of subList to allSubjectsArray
 		allSubjectsArray = subList.toArray();
 		
-		//set duration of all exams to 20minutes
+		//set duration of all exams to 10minutes
 		for (int i = 0; i < allSubjectsArray.length; i++) {
 			allSubjectsArray[i].setExamLength(10);}
 		
+		//Kubaner gets GDI and OOT
 		arraySubjectsKubaner[0] = allSubjectsArray[2];
 		arraySubjectsKubaner[1] = allSubjectsArray[3];
+		//Todorov gets ANA
 		arraySubjectsTodorov[0] = allSubjectsArray[1];
+		//Schramm gets ADS and TPE
 		arraySubjectsSchramm[0] = allSubjectsArray[0];
 		arraySubjectsSchramm[1] = allSubjectsArray[4];
+		
+		//creating 5 students
+		//student 1 ANA, OOT, TPE
 		arraySubjectsStu1[0] = allSubjectsArray[1];
 		arraySubjectsStu1[1] = allSubjectsArray[3];
 		arraySubjectsStu1[2] = allSubjectsArray[4];
+		//student 2 GDI, TPE
 		arraySubjectsStu2[0] = allSubjectsArray[2];
 		arraySubjectsStu2[1] = allSubjectsArray[4];
+		//student 3 ADS, OOT, TPE, GDI
 		arraySubjectsStu3[0] = allSubjectsArray[0];
 		arraySubjectsStu3[1] = allSubjectsArray[3];
 		arraySubjectsStu3[2] = allSubjectsArray[4];
 		arraySubjectsStu3[3] = allSubjectsArray[2];
+		//student 4 ANA
 		arraySubjectsStu4[0] = allSubjectsArray[1];
+		//student 5 ANA, GDI, OOT, TPE, ADS
 		arraySubjectsStu5[0] = allSubjectsArray[0];
 		arraySubjectsStu5[1] = allSubjectsArray[1];
 		arraySubjectsStu5[2] = allSubjectsArray[2];
 		arraySubjectsStu5[3] = allSubjectsArray[3];
 		arraySubjectsStu5[4] = allSubjectsArray[4];
 		
+		//creating timeperiod
+		//time 1 from 0:00 until 10:00
 		TimePeriod[] time1 = new TimePeriod[] { new TimePeriod(new Time(0, 0),new Time(10, 0)) };
-		TimePeriod[] time2 = time1;
+
 		
 		profList.create("Kubaner", arraySubjectsKubaner, time1);
 		profList.create("Todorov", arraySubjectsTodorov, time1);
 		profList.create("Schramm", arraySubjectsSchramm, time1);
-		stuList.create("Student 1", arraySubjectsStu1, time2);
-		stuList.create("Student 2", arraySubjectsStu2, time2);
-		stuList.create("Student 3", arraySubjectsStu3, time2);
-		stuList.create("Student 4", arraySubjectsStu4, time2);
-		stuList.create("Student 5", arraySubjectsStu5, time2);
+		stuList.create("Student 1", arraySubjectsStu1, time1);
+		stuList.create("Student 2", arraySubjectsStu2, time1);
+		stuList.create("Student 3", arraySubjectsStu3, time1);
+		stuList.create("Student 4", arraySubjectsStu4, time1);
+		stuList.create("Student 5", arraySubjectsStu5, time1);
+		
 		plan = alpha.generatePlan(new Time(10, 0));
 	}
 
@@ -85,11 +106,19 @@ public class PlanGenTest {
 	}
 
 	@Test
+	/**
+	 * Check for the quantity of used rooms
+	 * @throws Exception
+	 */
 	public void RoomCountTest() throws Exception {
-		assertEquals(1, plan.getRoomCount());
+		assertEquals(5, plan.getRoomCount());
 	}
 
 	@Test
+	/**
+	 * Check for the quantity of created exams
+	 * @throws Exception
+	 */
 	public void ExamCountTest() throws Exception {
 		int counter = 0;
 		for (int i = 0; i < plan.getTimeLineNumber(); i++) {
@@ -111,29 +140,33 @@ public class PlanGenTest {
 	}
 
 	@Test
+	/**
+	 * Check if the professor of an created exam can test this subject
+	 * @throws Exception
+	 */
 	public void ProfExamTest() throws Exception {
-		String profName = "";
+		Professor profName = null;
 		// move through every timeline
 		for (int g = 0; g < plan.getTimeLineNumber(); g++) {
 			for (int h = 0; h < plan.getTimeLine(g).size(); h++) {
 				// cast from timeline to exam object
 				try {
 					Exam exam = (Exam) plan.getTimeLine(g).getTimeLineMember(h);
-					// get name from professor of exam...
-					String tmp = (exam.getProfArray()[0].getName());
+					// get name from professor of exam... (for comparison!)
+					Professor tmp = (exam.getProfArray()[0]);
+					
 					// ...check if correct:
 					for (int i = 0; i < allSubjectsArray.length; i++) {
 						// if exam subject exists in arrayAll (contains all subjects)
 						if (exam.getSubjectArray()[0].equals(allSubjectsArray[i])) {
-							// check profList for...
+							// search in professor list for...
 							for (int j = 0; j < profList.toArray().length; j++) {
 								// ...specific subjects of professor
-								for (int k = 0; k < profList.toArray()[j]
-										.getSubjectArray().length; k++) {
+								for (int k = 0; k < profList.toArray()[j].getSubjectArray().length; k++) {
 									// if subject name in arrayAll at pos i equals subject of specific professor at pos j
 									if (allSubjectsArray[i].equals(profList.toArray()[j].getSubjectArray()[k])) {
 										// set profName as professor at pos j
-										profName = profList.toArray()[j].getName();
+										profName = profList.toArray()[j];
 									}
 								}
 							}
@@ -148,8 +181,12 @@ public class PlanGenTest {
 	}
 
 	@Test
+	/**
+	 * Check if student is tested in the right subject
+	 * @throws Exception
+	 */
 	public void StudExamTest() throws Exception {
-		String stuName = "";
+		Student stuName = null;
 		
 			// move through every timeline
 			for (int g = 0; g < plan.getTimeLineNumber(); g++) {
@@ -157,28 +194,30 @@ public class PlanGenTest {
 					// cast from timeline to exam object
 					try {
 					Exam exam = (Exam) plan.getTimeLine(g).getTimeLineMember(h);
-					// get name of student from exam...
-					String tmp = (exam.getStudent().getName());
+					// get name of student from exam... (for comparison!)
+					Student tmp = (exam.getStudent());
+					
 					// ...check if correct:
 					for (int i = 0; i < allSubjectsArray.length; i++) {
-
+						// if exam subject exists in arrayAll (contains all subjects)
 						if (exam.getSubjectArray()[0].equals(allSubjectsArray[i])) {
-
+							//search in student list for...
 							for (int j = 0; j < stuList.toArray().length; j++) {
-
+								//...the specific student, if found:
 								if (stuList.toArray()[j].equals(exam.getStudent())) {
-
+									//check subjects of student
 									for (int k = 0; k < stuList.toArray()[j].getSubjectArray().length; k++) {
-
+										//if there is a match...
 										if (stuList.toArray()[j].getSubjectArray()[k].equals(exam.getSubjectArray()[0])) {
-
-											stuName = stuList.toArray()[j].getName();
+											//...set stuName as found name
+											stuName = stuList.toArray()[j];
 										}
 									}
 								}
 							}
 						}
 					}
+					// check if student is tested in this subject by comparing name of student of exam we got and student we checked
 					assertEquals(stuName, tmp);
 				} catch (Exception e) {
 				}
@@ -187,6 +226,10 @@ public class PlanGenTest {
 	}
 	
 	@Test
+	/**
+	 * Check if there is a duplicate of an exam
+	 * @throws Exception
+	 */
 	public void DoubleExamTest() throws Exception {
 		//move through every timeline
 		for (int g = 0; g < plan.getTimeLineNumber(); g++) {
@@ -197,9 +240,9 @@ public class PlanGenTest {
 						for (int j = 0; j < plan.getTimeLine(i).size(); j++) {
 							try {
 								Exam tmp = (Exam) plan.getTimeLine(i).getTimeLineMember(j);
-								//compares every exam with the others, if g / h matches i / j we have a double one, fo the test fails
+								//compares every exam with the others, if g / h matches i / j we have a double one, so the test fails
 								if (exam.equals(tmp)&& plan.getTimeLine(g).getTimeLineMember(h) != plan.getTimeLine(i).getTimeLineMember(j)) {
-									fail("double!");
+									fail("double exam!");
 								}
 							} catch (Exception e) {
 							}
@@ -212,6 +255,10 @@ public class PlanGenTest {
 	}
 	
 	@Test
+	/**
+	 * Checks the length of an double exam
+	 * @throws Exception
+	 */
 	public void DoubleExamTimeTest() throws Exception {
 		for (int g = 0; g < plan.getTimeLineNumber(); g++) {
 			for (int h = 0; h < plan.getTimeLine(g).size(); h++) {
@@ -229,6 +276,10 @@ public class PlanGenTest {
 	}
 	
 	@Test
+	/**
+	 * Check if there is an assessor
+	 * @throws Exception
+	 */
 	public void AssessorExistTest() throws Exception {
 		for (int g = 0; g < plan.getTimeLineNumber(); g++) {
 			for (int h = 0; h < plan.getTimeLine(g).size(); h++) {
@@ -242,6 +293,10 @@ public class PlanGenTest {
 	}
 	
 	@Test
+	/**
+	 * Check if assessor == the testing professor
+	 * @throws Exception
+	 */
 	public void AssessorProfTest() throws Exception {
 		for (int g = 0; g < plan.getTimeLineNumber(); g++) {
 			for (int h = 0; h < plan.getTimeLine(g).size(); h++) {
@@ -254,4 +309,35 @@ public class PlanGenTest {
 		}
 	}
 
+	@Test
+	/**
+	 * Check if student is tested in all his subjects 
+	 * by counting all his exams and comparing with the quantity of subjets
+	 * @throws Exception
+	 */
+	public void StudAllExamTestedTest() throws Exception {
+		for (int i = 0; i < stuList.size(); i++) {
+			int counter = 0;
+			Student student = stuList.get(i);
+			for (int g = 0; g < plan.getTimeLineNumber(); g++) {
+				for (int h = 0; h < plan.getTimeLine(g).size(); h++) {
+					try {
+						Exam exam = (Exam) plan.getTimeLine(g).getTimeLineMember(h);
+						if (exam.getStudent().equals(student)) {
+							for (int j = 0; j < student.getSubjectArray().length; j++) {
+								if (exam.getSubjectArray()[0].equals(student.getSubjectArray()[j])) {
+									counter++;
+								}
+								if (exam.isDoubleExam() && exam.getSubjectArray()[1].equals(student.getSubjectArray()[j])) {
+									counter++;
+								}
+							}
+						}
+					} catch (Exception f) {
+					}
+				}
+			}
+			assertEquals(student.getSubjectArray().length, counter);
+		}
+	}
 }
